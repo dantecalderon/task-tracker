e7e6ba5e9ff51b75dd79d9299d3b1885fb29b763
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2024-12-08 14:35:30 -0500

    Add list tasks

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
 #!/usr/bin/env -S node --experimental-strip-types --experimental-transform-types --no-warnings
 
-import { randomUUID } from 'node:crypto';
-import { readFileSync } from 'fs';
+import { writeFileSync, readFileSync } from 'node:fs';
 
-const DB_FILENAME = './tasks.json';
+const DB_FILENAME = './my_tasks.json';
 
 enum TaskStatus {
   Todo = 'todo',
@@ -30,30 +29,69 @@ enum Command {
 }
 
 
-function readTasksFromFile() {
-  const file = readFileSync(DB_FILENAME);
+function readTasksFromFile(): Task[] {
+  try {
+    const fileRaw = readFileSync(DB_FILENAME);
 
-  console.log(file)
+    return JSON.parse(fileRaw.toString());
+  } catch (error) {
+
+    console.warn('Warn: ', error.message);
+    console.log('Starting a new list...');
+
+    return [];
+  }
 }
 
-
+function writeTasksFile(tasks: Task[]) {
+  try {
+    writeFileSync(DB_FILENAME, JSON.stringify(tasks));
+  } catch (error) {
+    console.error('Error: ', `Error writting tasks to file ${DB_FILENAME}`)
+  }
+}
 
 function listTasks(status?: TaskStatus) {
+  const tasks = readTasksFromFile()
 
+  if (status && !Object.values(TaskStatus).includes(status as TaskStatus)) {
+    throw new Error(`Task status provided is invalid, use one of the next: ${Object.values(TaskStatus).join(', ')}`)
+  }
+
+  const filteredTasks = status ? tasks.filter(task => task.status === status) : tasks;
+
+  console.table(filteredTasks);
 }
 
+
+const commandsListener: Partial<Record<Command, Function>> = {
+  list: listTasks,
+}
 
 function main() {
   try {
     const command = process.argv[2];
 
-    if (!(command in Command)) {
+    console.log('Command', command)
+
+    if (!Object.values(Command).includes(command as Command)) {
       throw new Error(`Command is not valid, use one of the next: ${Object.values(Command).join(', ')}`)
     }
 
+    const listener = commandsListener[command];
 
+    if (!listener) {
+      throw new Error(`Listener action not defined for ${command} command.`)
+    }
 
+    const commandArgs = process.argv.slice(3);
 
+    console.debug('Executing command: ', {
+      command,
+      commandArgs
+    });
+
+    listener(...commandArgs);
 
   } catch(error) {
     console.error("Error: ", error.message);
